1539979dfbeb7b60352267b63b5e527eab2ce1cb
Mateus-vonRandow <mateusvonrandowmiranda@gmail.com>
2026-04-13 19:48:37 -0300

    feat: init components header/footer/layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-white border-t border-t-2 p-4 text-center">
+    <p>Criado por Mateus von Randow. O código fonte está no <a class="underline" href="https://github.com/Mateus-vonRandow/habit-tracker" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<header class="bg-white border-b-2 flex items-center justify-between p-4">
+    {{-- logo --}}
+    <div>
+        logo
+    </div>
+
+    {{-- github --}}
+    <div>
+        github
+    </div>
+</header>
