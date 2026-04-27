f0dcf05952895029efdc4fa29123fa705268d18b
Mateus-vonRandow <mateusvonrandowmiranda@gmail.com>
2026-04-27 15:21:10 -0300

    feat(Auth): implement LoginRequest for validation and update authentication logic

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
@@ -15,12 +16,9 @@ class LoginController extends Controller
     }
 
 
-    public function authenticate(Request $request)
+    public function authenticate(LoginRequest $request)
     {
-        $credentials = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required|min:6',
-        ]);
+        $credentials = $request->only('email', 'password');
 
         if(Auth::attempt($credentials)) {
             $request->session()->regenerate();
